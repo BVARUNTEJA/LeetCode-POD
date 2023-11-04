61ed79ade321b6cd7bc4d0085ff33de5f50b1ec7
BVARUNTEJA <87554717+BVARUNTEJA@users.noreply.github.com>
2023-11-04 09:39:28 +0530

    Update 03-11-2023.py

--- 03-11-2023.py
+++ 03-11-2023.py
@@ -3,12 +3,16 @@ class Solution:
         ans=[]
         j=0
         for i in range(1,n+1):
+            # reached the end
             if j>=len(target):
                 break
+            # first append the number to check
             ans.append('Push')
             if target[j]==i:
                 j+=1
+                # if number are equal then no need to pop the number
                 continue
+            #  pop the number which we append recently
             ans.append('Pop')
         return ans
 
